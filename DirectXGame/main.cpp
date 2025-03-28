8d0df30cc92bb6f21a7281f58264c0de8f48f775
AkiraWatanabeNeec <watanabekr@g.neec.ac.jp>
2025-03-28 16:09:36 +0900

    基本コード を適用した 01-01 p.19

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,17 @@
+#include <KamataEngine.h>
 #include <Windows.h>
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	
+	// エンジンの初期化
+	KamataEngine::Initialize(L"LE2C_00_ワタナベ_アキラ");
+	while (true) {
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+	}
+	// エンジンの終了処理
+	KamataEngine::Finalize();
 	return 0;
 }
