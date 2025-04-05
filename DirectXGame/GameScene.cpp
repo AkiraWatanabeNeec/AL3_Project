27160d87fc93ee74648222aef7db68146000bebc
AkiraWatanabeNeec <watanabekr@g.neec.ac.jp>
2025-04-05 17:24:16 +0900

    3d line の描画をためした

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -1,5 +1,6 @@
 #include "GameScene.h"
 #include "2d\ImGuiManager.h"
+#include "3d\PrimitiveDrawer.h"
 
 using namespace KamataEngine;
 
@@ -14,7 +15,10 @@ void GameScene::Initialize() {
 
 	// サウンドデータの読み込み
 	soundDataHandle_ = Audio::GetInstance()->LoadWave("fanfare.wav");
-	voiceHandle_ = Audio::GetInstance()->PlayWave(soundDataHandle_, true, 0.5f);
+	voiceHandle_ = Audio::GetInstance()->PlayWave(soundDataHandle_, true, 0.1f);
+
+	// ライン描画テスト : カメラのアドレスを渡す
+	PrimitiveDrawer::GetInstance()->SetCamera(&camera_);
 
 }
 
@@ -53,16 +57,21 @@ void GameScene::Draw() {
 	Model::PreDraw(dxCommon->GetCommandList());
 
 	// 3Dモデル描画
+	//PrimitiveDrawer::GetInstance()->DrawLine3d({0,0,0},{10,0,0},{1.0f, 0.0f, 0.0f, 1.0f});   // エラーになった
 	model_->Draw(worldTransform_, camera_, textureHandle_);
+
+	//PrimitiveDrawer::GetInstance()->DrawLine3d({0,0,0},{10,0,0},{1.0f, 0.0f, 0.0f, 1.0f});   // モデル描画の前で大丈夫だったが…
 
 	// 3Dモデル描画後処理
 	Model::PostDraw();
+
+	PrimitiveDrawer::GetInstance()->DrawLine3d({0,0,0},{10,0,0},{1.0f, 0.0f, 0.0f, 1.0f});
 
 	// スプライト描画前処理
 	Sprite::PreDraw(dxCommon->GetCommandList());
 
 	// スプライトインスタンスの描画処理
-	sprite_->Draw();
+	//sprite_->Draw();
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
